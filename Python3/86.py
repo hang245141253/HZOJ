46699bdb8a9c9fc98977fa14421a95a1ce103fb8
hang245141253 <hang245141253@gmail.com>
2023-04-28 19:31:05 +0800

    Python3

---
+++ Python3/86.py
@@ -0,0 +1,6 @@
+a, b, c = input().split()   # split()函数默认用空格来间隔多个参数输入
+a = int(a)
+b = int(b)
+c = int(c)
+
+print(a * b * c)
---
+++ Python3/87.py
@@ -0,0 +1,7 @@
+a, b = input().split()
+a = float(a)
+b = float(b)
+
+# "{ ... }".format( ... )
+print("{:.2f}".format(2 * a + 2 * b))
+print("{:.2f}".format(a * b))
