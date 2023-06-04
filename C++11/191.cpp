92f33efc75d50c86a098e08b388cbd2557f7a26e
hang245141253 <hang245141253@gmail.com>
2023-06-04 18:30:39 +0800

    2023/6/4

--- C++11/191.cpp
+++ C++11/191.cpp
@@ -17,12 +17,7 @@ int get_index(int *arr, int l) {
     return 0;
 }
 
-int main() {
-    int l, r;
-    scanf("%d%d", &l, &r);
-    int *arr = new int[r + 1]();
-    init(arr, r);
-
+void solve(int *arr, int l) {
     int min = 0x0fffffff, max = 0;
     int min_index = 0, max_index = 0;
     for (int i = get_index(arr, l); i < arr[0]; i++) {
@@ -35,8 +30,20 @@ int main() {
             max_index = i;
         }
     }
-    if (arr[0] - get_index(arr, l) > 1) printf("%d,%d are closest, %d,%d are most distant.\n", arr[min_index], arr[min_index + 1], arr[max_index], arr[max_index + 1]);
-    else printf("There are no adjacent primes.\n");
+    if (arr[0] - get_index(arr, l) > 1) 
+        printf("%d,%d are closest, %d,%d are most distant.\n",
+         arr[min_index], arr[min_index + 1],
+         arr[max_index], arr[max_index + 1]);
+    else 
+        printf("There are no adjacent primes.\n");
+}
+
+int main() {
+    int l, r;
+    scanf("%d%d", &l, &r);
+    int *arr = new int[r + 1]();
+    init(arr, r);
+    solve(arr, l);
     
     return 0;
 }
