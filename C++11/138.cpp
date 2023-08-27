73fc640d341769b166dcf18e7b9fd5ccd38b4129
ゞ灰酱 <hang245141253@gmail.com>
2023-08-27 14:45:12 +0800

    2023/8/27

--- C++11/138.cpp
+++ C++11/138.cpp
@@ -5,8 +5,8 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    for (int i = 0; i < n; i++) {
-        for (int j = 2 * i; j < 2 * n; j++) {
+    for (int i = n; i >= 1; i--) {
+        for (int j = 0; j < 2 * i; j++) {
             cout << "A";
         }
         cout << endl;
