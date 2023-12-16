a47c23aafc06774acafe944f1451f5e1ce8c0ba6
hang245141253 <hang245141253@gmail.com>
2023-12-16 17:52:48 +0800

    2023/12/16

--- C++11/168.cpp
+++ C++11/168.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
+#include <algorithm>
+using namespace std;
+
+string name[105];
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> name[i];
+        for (int j = 0; j < name[i].length(); j++)
+            name[i][j] = tolower(name[i][j]);
+        name[i][0] -= 32;
+    }
+
+    sort(name, name + n);
+    for (int i = 0; i < n; i++) {
+        cout << name[i] << endl;
+    }
+
+    return 0;
+}
+
+/*
+#include <iostream>
 #define swap(a, b) {\
-	string tmp=a;\
-	a=b;b=tmp;\
+    string tmp=a;\
+    a=b;b=tmp;\
 }
 
 using namespace std;
@@ -20,23 +45,24 @@ int main() {
             if (str[i][j] > 64 && str[i][j] < 91)   str[i][j] += 32;
         }
     }
-/*
-    // 冒泡排序
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (str[j].compare(str[j + 1]) > 0) swap(str[j], str[j + 1]);
-        }
-    }
-*/
-    // 插入排序
-    for (int i = 1; i < n; i++) {
-        for (int j = i; j > 0 && str[j].compare(str[j - 1]) < 0; j--) {
-            swap(str[j], str[j - 1]);
-        }
-    }
 
-    for (int i = 0; i < n; i++) {
-        cout << str[i] << endl;
+    // // 冒泡排序
+    // for (int i = 0; i < n - 1; i++) {
+    //     for (int j = 0; j < n - i - 1; j++) {
+    //         if (str[j].compare(str[j + 1]) > 0) swap(str[j], str[j + 1]);
+    //     }
+    // }
+
+// 插入排序
+for (int i = 1; i < n; i++) {
+    for (int j = i; j > 0 && str[j].compare(str[j - 1]) < 0; j--) {
+        swap(str[j], str[j - 1]);
     }
-    return 0;
 }
+
+for (int i = 0; i < n; i++) {
+    cout << str[i] << endl;
+}
+return 0;
+}
+*/
