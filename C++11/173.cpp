0caea068211c9a7c993a3f0a50b4a0055d9dedc6
hang245141253 <hang245141253@gmail.com>
2023-12-16 17:33:50 +0800

    2023/12/16

--- C++11/173.cpp
+++ C++11/173.cpp
@@ -3,13 +3,17 @@ using namespace std;
 
 int main() {
     string str;
-    getline(cin, str);  //getline可以接收空格
+    getline(cin, str);  // getline可以接收空格
     int abc = 0, num = 0, space = 0, other = 0;
     for (int i = 0; i < str.size(); i++) {
-        if (str[i] > 64 && str[i] < 91 || str[i] > 96 && str[i] < 123)  abc++;
-        else if (str[i] > 47 && str[i] < 58)    num++;
-        else if (str[i] == 32)  space++;
-        else    other++;
+        if (str[i] >= 'A' && str[i] <= 'Z' || str[i] >= 'a' && str[i] <= 'z')
+            abc++;
+        else if (str[i] >= '0' && str[i] <= '9')
+            num++;
+        else if (str[i] == ' ')
+            space++;
+        else
+            other++;
     }
     cout << abc << " " << num << " " << space << " " << other << endl;
     return 0;
